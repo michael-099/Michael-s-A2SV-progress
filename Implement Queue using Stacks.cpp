7429a55a5aa425ff01c7399578e83faa77ee6704
michael shimeles <105845333+michael-099@users.noreply.github.com>
2022-11-17 08:05:18 +0300

    Create Implement Queue using Stacks.cpp

---
+++ Implement Queue using Stacks.cpp
@@ -0,0 +1,80 @@
+class MyQueue {
+public:
+    stack <int> one ;
+        stack <int> two;
+        int count =0;
+       int a,b,c,o,x,y;
+    MyQueue() {  
+       
+    }
+    
+    void push(int x) {
+        one.push(x);
+        count++;
+        
+    }
+    
+    int pop() {
+        for(int i=0 ;i<count ;i++){
+            a = one.top();
+                one.pop();
+            two.push(a);
+        }
+        c=two.top();
+         two.pop();
+        count --;
+        for(int i=0;i<count;i++){
+            b=two.top();
+                two.pop();
+          //  cout<<b;
+            one.push(b);
+        }
+        
+        return c;
+        
+    }
+    
+    int peek() {
+           
+           for (int j=0;j<count;j++){
+             o=one.top();
+              one.pop();
+             two.push(o);
+               x=two.top();
+         }
+        for (int k=0;k<=count-1;k++){
+            y=two.top();
+            two.pop();
+            one.push(y);
+        }
+              
+         return x;
+    //     for(int i=0 ;i<count-1 ;i++){
+    //         o = one.top();
+    //             one.pop();
+    //         two.push(a);
+    //     }
+    //     int x=two.top();
+    //   return  x;  
+        // while(one.top() != NULL)
+        // {cout << one.top();
+        // one.pop();}
+        //int x=0 ;
+        
+       //  return one.top();
+    }
+    
+    bool empty() {return one.empty();
+        
+        
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
