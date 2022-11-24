e69beff3aef81ef42e04ec8e9f074e9f4ac26c8b
michael shimeles <105845333+michael-099@users.noreply.github.com>
2022-11-24 09:14:29 +0300

    Create Evaluate Reverse Polish Notation.java

---
+++ Evaluate Reverse Polish Notation.java
@@ -0,0 +1,57 @@
+
+class Solution {
+ public
+  int evalRPN(String[] tokens) {
+    Stack<Integer> stack = new Stack<>();
+     int x, y, z,a,b;
+    int o = 0;
+    for (int i = 0; i < tokens.length; i++) {
+      if (!tokens[i].equals("+") && !tokens[i].equals("-") &&
+          !tokens[i].equals("*") && !tokens[i].equals("/")) {
+        stack.push(Integer.parseInt(tokens[i]));
+      }
+   
+    
+      if (tokens[i].equals ("+")) {
+      // tokens
+          a = stack.peek();
+        stack.pop();
+        b = stack.peek();
+        stack.pop();
+        o = a + b;
+        stack.push(o);
+      }
+      if (tokens[i].equals("-")) {
+        a = stack.peek();
+        stack.pop();
+        b = stack.peek();
+        stack.pop();
+        o = b - a;
+        stack.push(o);
+      }
+      if (tokens[i].equals( "*")) {
+        a = stack.peek();
+        stack.pop();
+        b = stack.peek();
+        stack.pop();
+        o = a * b;
+        stack.push(o);
+      }
+      if (tokens[i].equals ("/")) {
+        a = stack.peek();
+        stack.pop();
+        b = stack.peek();
+        stack.pop();
+        o = b / a;
+        stack.push(o);
+      }
+    }
+    // while (stack != null) {
+    //   System.out.println(stack.peek());
+    //   stack.pop();
+    // }
+    return stack.peek();
+  }
+}
+     
+     
