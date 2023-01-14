867cb4b026a58da7336b41127307bdba983c13f1
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-01-14 15:53:28 +0300

    Create Delete Node in a Linked List.cpp

---
+++ Delete Node in a Linked List.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+       ListNode* temp = node ->next;
+       node->val = temp->val;
+       node-> next=temp->next;
+       delete temp;
+     
+    
+    
+        // while(temp->next!= node){
+        //    temp2=temp;
+        //     temp = temp->next;
+        //      }
+        //      temp2->next=temp->next;
+        //      delete temp;
+
+    
+        
+        
+    }
+};
