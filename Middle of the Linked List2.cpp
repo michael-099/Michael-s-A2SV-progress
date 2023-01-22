9725ffc2a325fb71af0f2f9bf0acbab9f61de00b
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-01-22 11:46:04 +0300

    Create Middle of the Linked List2.cpp

---
+++ Middle of the Linked List2.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int middle;
+        int count=1;
+        int chake=1;
+        ListNode* temp=head->next;
+
+        while( temp!=NULL){
+            temp = temp->next;
+            count++;
+           // cout<<"done";
+            cout<< count<<","<<endl;
+           cout <<endl;
+        }
+        // if (count%2!=0){
+        //     count++;
+        // }
+        middle=ceil(count/2)+1;
+        cout<<middle;
+        temp=head;
+        while(temp!= NULL && middle!=chake){
+            temp=temp->next;
+            chake++;
+           // cout<<"done2";
+
+
+        }
+        return temp;
+    }
+
+};
+Console
