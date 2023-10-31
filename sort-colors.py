b72c90773175f371977fc91788ab9d505f7e80b7
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-10-31 08:29:41 +0300

    added sort-colors.python

--- sort-colors.py
+++ sort-colors.py
@@ -1,12 +1,29 @@
 class Solution:
+    
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(1,len(nums)):
-            key=nums[i]
-            j=i-1
-            while j>=0 and key<nums[j]:
-                nums[j+1]=nums[j]
-                j=j-1
-            nums[j+1]=key
+        # for i in range(1,len(nums)):
+        #     key=nums[i]
+        #     j=i-1
+        #     while j>=0 and key<nums[j]:
+        #         nums[j+1]=nums[j]
+        #         j=j-1
+        #     nums[j+1]=key
+        start =0 
+        end=len(nums)-1 
+        middle=0
+        temp=0
+       
+        while end>= middle:
+            if nums[middle]==0:
+               nums[middle],nums[start]=nums[start],nums[middle]
+               start=start+1 
+               middle=middle +1
+            elif nums[middle]==1:
+                middle=middle+1
+            else:
+               nums[middle],nums[end]=nums[end],nums[middle]
+               end=end-1    
+        return nums
