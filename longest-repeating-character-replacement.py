be241367cdee0f05b7955b732218d772ad015f04
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-02 09:12:10 +0300

    added longest-repeating-character-replacement.python

--- longest-repeating-character-replacement.py
+++ longest-repeating-character-replacement.py
@@ -6,8 +6,8 @@ class Solution:
             dic[s[end]]=dic.get(s[end],0)+1 
             max_fr = max(max_fr,dic[s[end]])
             while (end-start+1)-max_fr > k:
-                start=start+1
                 dic[s[start]]=dic[s[start]]-1 
+                start=start+1
             end = end+1 
             res=max(res,(end-start))
         return res
